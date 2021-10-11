bc7f936a433afc4c9c80d73c43bf1ae13d06fd39
tkhalilo <tkhalilo@estee.com>
2021-10-11 12:40:52 +0300

    make it empty

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,32 +1,5 @@
-def show_image():
-    pass
-
-
-def make_move():
-    show_field()
-    x, y = input(), input()
-    #is correct input
-    #is mine
-
-
-def generate_field():
-    pass
-
-
-def is_finish():
-    pass
-
-
-def start_game():
-    field, image = generate_grid()
-    while make_move(field):
-        pass
-
-
-def is_new_game():
-    pass
-
-
 def main():
-    while is_new_game():
-        start_game()
+    pass
+
+
+main()
