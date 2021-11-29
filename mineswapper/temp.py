97486c0e76fbd7723827342c18026672aaa692ee
CURROCHKA <iivgav12@gmail.com>
2021-11-29 21:11:53 +0300

    hw7(generate_field)(fixed_bug)

--- mineswapper/temp.py
+++ mineswapper/temp.py
@@ -1,13 +1,11 @@
+from random import shuffle
+
+
 def generate_field(num_of_rows: int, num_of_columns: int, mines_number: int) -> list:
-
-    from random import randint
-    from random import shuffle
-    field = [0] * (num_of_rows * num_of_columns)
-    print(shuffle(field))
-    #field = [[randint(0, 1) for i in range(num_of_rows)] for j in range(num_of_columns)]
     image = [['#' for i in range(num_of_rows)] for j in range(num_of_columns)]
+    field = [0] * (num_of_rows * num_of_columns - mines_number) + [1] * mines_number
+    field_test = [[0 for i in range(num_of_rows)] for j in range(num_of_columns)]
+    return field
 
-    return field, image
 
-
-print(generate_field(3, 3, 1))
+print(generate_field(2, 2, 2))
