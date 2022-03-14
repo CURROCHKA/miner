7532b0df519ffc80b5881e94d1f412f114b47ba1
Ivan <iivgav12@gmail.com>
2022-03-14 15:26:54 +0300

    Update code

--- Tic-Tac-Toe/button.py
+++ Tic-Tac-Toe/button.py
@@ -6,12 +6,13 @@ class Button:
     """Характеристики всех кнопок."""
     def __init__(self, t_game, x, y, rect_w, rect_h):
         self.settings = Settings()
-        self.who = t_game.who
         self.screen = t_game.screen
         self.button_color = self.settings.button_color
+        self.button_active = self.settings.button_active
         self.button = self.settings.button
-        self.button_rect = pygame.Rect(x, y, rect_w, rect_h)
+        self.who = self.settings.who
+        self.rect = pygame.Rect(x, y, rect_w, rect_h)
 
     def draw_button(self):
         self.button.fill(self.button_color)
-        self.screen.blit(self.button, self.button_rect)
+        self.screen.blit(self.button, self.rect)
--- Tic-Tac-Toe/settings.py
+++ Tic-Tac-Toe/settings.py
@@ -12,33 +12,6 @@ class Settings:
         # Настройки кнопок
         self.button = pygame.Surface((125, 125))
         self.button_color = (255, 255, 255)
+        self.button_active = True
+        self.who = ''
 
-
-
-
-        self.topleft_active = True
-        self.topleft_who = ''
-
-        self.center_active = True
-        self.center_who = ''
-
-        self.topright_active = True
-        self.topright_who = ''
-
-        self.top_active = True
-        self.top_who = ''
-
-        self.left_active = True
-        self.left_who = ''
-
-        self.bottomleft_active = True
-        self.bottomleft_who = ''
-
-        self.bottom_active = True
-        self.bottom_who = ''
-
-        self.bottomright_active = True
-        self.bottomright_who = ''
-
-        self.right_active = True
-        self.right_who = ''
