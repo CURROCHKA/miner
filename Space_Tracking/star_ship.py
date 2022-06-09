de714ed601cd524c7b0d3e01812a21c1e153b49e
Ivan <iivgav12@gmail.com>
2022-06-09 11:59:42 +0300

    grouped classes by files

--- Space_Tracking/star_ship.py
+++ Space_Tracking/star_ship.py
@@ -1,16 +1,7 @@
-from random import randint
+from space_tracking_main import player
 
 HEIGHT = 10
 WIDTH = 10
-
-
-class Player:
-    def __init__(self, name: str):
-        self.name = name
-        self.money = 2000000
-
-    def buy_ship(self, ship, planet):
-        pass
 
 
 class Engine:
@@ -153,96 +144,4 @@ class StarShip:
                 player.money += self.location.stock.products[product][1] * amount
             else:
                 print(f'У вас есть только {self.cargo[product]} {product.lower()}')
-
-
-class Stock:
-    def __init__(self):
-        self.products = {'food': [100, 10],
-                         'minerals': [1000, 20],
-                         'medicines': [100, 30],
-                         'materials': [100, 100],
-                         'fuel': [1000, 10],
-                         'appliances': [100, 50],
-                         'technic': [100, 80],
-                         'luxuries': [100, 100]
-                         }  # (0 - кол-во, 0 - цена)
-
-    def increase_products(self, product: str, quantity: int):
-        self.products[product][0] += quantity
-
-    def new_price(self, product: str, price: int):
-        self.products[product][1] = price
-
-
-class Shop:
-    def __init__(self):
-        self.ships = []
-        self.engines = []
-        self.tanks = []
-
-
-class Planet:
-    def __init__(self, name: str, planet_type: str):
-        self.name = name
-        self.planet_type = planet_type
-        self.stock = Stock()
-        self.shop = Shop()
-        self.coord = self.__generate_coord()
-
-    def __count_planets(self) -> int:
-        count = 0
-        for i in range(HEIGHT):
-            for j in range(WIDTH):
-                if [i, j] not in planets_coord:
-                    count += 1
-        return count
-
-    def __generate_coord(self) -> tuple:
-        while self.__count_planets() <= HEIGHT * WIDTH:
-            x, y = randint(0, HEIGHT - 1), randint(0, WIDTH - 1)
-            if [x, y] in planets_coord:
-                continue
-            planets_coord.append([x, y])
-            return x, y
-
-    def get_prices(self) -> dict:
-        prices = {}
-        for i in self.stock.products:
-            prices.update({i: self.stock.products[i][1]})
-        return prices
-
-
-planets_coord = []
-planet1 = Planet('Auropa', 'None')
-planet2 = Planet('Earth', 'None')
-planet3 = Planet('Mars', 'None')
-player = Player('Ivan')
-engine = Engine(2, 50)
-tank = Tank(100, 50)
-star_ship = StarShip('Buran', 100, planet2, engine, tank)
-print(star_ship.tank.fuel, 'Топливо')
-print(player.money)
-print(planet2.stock.products['fuel'], 'Топливо на складе')
-star_ship.refuel(1000)
-print(player.money)
-print(planet2.stock.products['fuel'], 'Топливо на складе')
-print(star_ship.tank.fuel, 'Топливо')
-print(star_ship.get_distance(planet1), 'Расстояние')
-star_ship.move_to_planet(planet1)
-print(star_ship.tank.fuel, 'Топливо после перелёта на др. планету')
-star_ship.refuel(10)
-print(star_ship.tank.fuel, 'Топливо после заправки')
-print(planet1.stock.products['fuel'], 'Топливо на складе')
-print(planet1.stock.products['food'], 'Еда на складе')
-print(player.money, 'Деньги игрока')
-print(star_ship.cargo['food'], 'Еда у игрока')
-star_ship.buy('food', 10)
-print('Купил еду')
-print(player.money, 'Деньги игрока после покупки еды')
-print(planet1.stock.products['food'], 'Еда на складе после покупки')
-print(star_ship.cargo['food'], 'Еда у игрока после покупки')
-star_ship.sale('food', 10)
-print('Продал еду')
-print(planet1.stock.products['food'], 'Еда на складе после продажи')
-print(star_ship.cargo['food'], 'Еда у игрока после продажи')
-print(player.money, 'Деньги игрока после продажи еды')
+                
