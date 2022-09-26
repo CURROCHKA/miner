b39a66f023b0dfa69a070021cb3017b77e8661da
Ivan <iivgav12@gmail.com>
2022-09-26 21:42:03 +0300

    26.09.22

--- Rubies/ruby.py
+++ Rubies/ruby.py
@@ -3,12 +3,22 @@ import pygame
 
 class Ruby:
     def __init__(self, rubies_game, color: str):
-        self.screen = rubies_game.screen
+        super().__init__()
+        self.rg = rubies_game
+        self.screen = self.rg.screen
         self.color = color
         self.image = pygame.image.load(f'images/{self.color}.png')
+        self.image = self.image.convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
+        self.id = None
+        self.id_simple = None
 
     def draw(self):
         self.screen.blit(self.image, self.rect)
+
+    def update_pos(self, x, y):
+        self.rect.x, self.rect.y = self.rect.x + x, self.rect.y + y
+
+    def new_color(self, color):
+        self.color = color
+        self.image = pygame.image.load(f'images/{self.color}.png')
