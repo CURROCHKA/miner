f64c967edd0e4208d33a7ac69216a7f6ee339c54
Ivan <iivgav12@gmail.com>
2022-10-15 23:33:08 +0300

    rewrite structure of classes

--- Space_Tracking/product.py
+++
@@ -1,5 +0,0 @@
-class Product:
-    def __init__(self, title):
-        self.title = title
-        self.price = 0
-        self.amount = 0
--- Space_Tracking/refactoring/star_ship2.py
+++ Space_Tracking/refactoring/star_ship2.py
@@ -1,5 +1,5 @@
 from math import sqrt
-from planet2 import Planet, Product
+from refactoring.planet2 import Planet
 
 
 class Engine:
@@ -19,32 +19,54 @@ class StarShip:
         self.name = name
         self.location = location
         self.cargo_capacity = capacity
-        self.cargo = [Product(title) for title in
-                      ['mineral', 'medicine', 'food', 'material', 'appliance', 'technic', 'luxury']]
+        self.cargo = {'minerals': 0,
+                      'medicines': 0,
+                      'food': 0,
+                      'materials': 0,
+                      'appliances': 0,
+                      'technic': 0,
+                      'luxuries': 0
+                      }
         self.engine = engine
         self.tank = tank
-        self.system = StarShip.StarShipSystem(self)
-        self.control_module = StarShip.ControlModule(self)
+        self.system = ShipSystem(self)
 
     def move_to_planet(self, planet: Planet):
         if planet != self.location:
-            distance = self.system.get_distance(planet)
+            distance = self.system.navigation_module.get_distance(planet)
             if self.engine.speed * distance <= self.tank.fuel:
                 self.location = planet
                 self.tank.fuel -= self.engine.speed * distance
 
-    class StarShipSystem:
+
+class ShipSystem:
+    def __init__(self, star_ship):
+        self.ship = star_ship
+        self.cargo_module = ShipSystem.CargoModule(self.ship)
+        self.navigation_module = ShipSystem.NavigationModule(self.ship)
+        self.control_module = ShipSystem.ControlModule(self.ship)
+
+    class CargoModule:
         def __init__(self, star_ship):
             self.ship = star_ship
-            self.location = self.ship.location
 
         @property
         def current_capacity(self):
             return sum([product.amount for product in self.ship.cargo])
 
+    class NavigationModule:
+        def __init__(self, star_ship):
+            self.ship = star_ship
+            self.location = self.ship.location
+
         def get_distance(self, planet: Planet):
             x, y = (planet.coord[0] - self.location.coord[0]), (planet.coord[1] - self.location.coord[1])
             return round(sqrt(x ** 2 + y ** 2))
+
+    class ControlModule:
+        def __init__(self, star_ship):
+            self.ship = star_ship
+            self.location = self.ship.location
 
         @staticmethod
         def is_valid_fuel(fuel: int) -> bool:
@@ -54,19 +76,14 @@ class StarShip:
             return False
 
         def is_possible_refuel(self, fuel: int) -> bool:
-            if self.location.stock.system.get_product('fuel').amount >= fuel:
+            if self.location.stock.system.get_product('fuel')[0] >= fuel:
                 return True
             return False
 
-    class ControlModule:
-        def __init__(self, star_ship):
-            self.ship = star_ship
-            self.location = self.ship.location
-
         def refuel(self, fuel: int):
-            if self.ship.system.is_valid_fuel(fuel) and self.ship.system.is_possible_refuel(fuel):
+            if self.is_valid_fuel(fuel) and self.is_possible_refuel(fuel):
                 if fuel + self.ship.tank.fuel <= self.ship.tank.capacity:
-                    self.location.stock.system.get_product('fuel').amount -= fuel
+                    self.location.stock.system.get_product('fuel')[0] -= fuel
                     self.ship.tank.fuel += fuel
                 else:
                     self.ship.tank.fuel += self.ship.tank.capacity - self.ship.tank.fuel
@@ -81,13 +98,7 @@ class StarShip:
 planet1 = Planet('Earth')
 planet2 = Planet('Auropa')
 star_ship1 = StarShip('qwerty', 100, planet1, Engine(1), Tank(100))
-planet1.stock.system.update_amount('fuel', 100)
-print(star_ship1.tank.fuel)
-star_ship1.control_module.refuel(10)
-print(star_ship1.tank.fuel)
+star_ship1.system.control_module.refuel(100)
 print(star_ship1.location.name)
 star_ship1.move_to_planet(planet2)
 print(star_ship1.location.name)
-print(star_ship1.tank.fuel)
-star_ship1.control_module.refuel(90)
-print(star_ship1.tank.fuel)
