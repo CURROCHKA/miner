f7ab92421023cace51ebf763cda9f63d319b59f1
Ivan <iivgav12@gmail.com>
2022-10-23 19:23:36 +0300

    commit

---
+++ Tic-Tac-Toe/field.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class Field:
+    """Поле игры"""
+
+    def __init__(self, t_game):
+        # Главный экран
+        self.screen = t_game.screen
+        self.screen_rect = self.screen.get_rect()
+        # Изображение поля
+        self.image = pygame.image.load('image/field.png')
+        self.rect = self.image.get_rect()
+        # Позиция поля
+
+    def update(self):
+        self.screen.blit(self.image, self.rect)
+        self.rect.center = self.screen_rect.center
