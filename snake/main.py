180252d43d41780a6e229aa928d2e686b4bcc6bf
Ivan <iivgav12@gmail.com>
2024-03-03 19:58:09 +0300

    generate fruit coords

--- snake/main.py
+++ snake/main.py
@@ -1,13 +1,18 @@
 import sys
 import pygame
+from random import randrange
 from snake import Snake
+from fruit import Fruit
 
 
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.screen_rect = self.screen.get_rect()
+        self.screen_size = self.screen.get_size()
+        self.cell_size = (25, 25)
         self.snake = Snake(self.screen_rect)
+        self.fruit = Fruit(self.create_fruit_coord(), self.cell_size)
         self.clock = pygame.time.Clock()
         self.frame = 5
         pygame.display.set_caption('Snake')
@@ -18,15 +23,29 @@ class Game:
             self.snake.update()
             self.update_screen()
 
+    def create_fruit_coord(self) -> tuple:
+        space_x = int(self.screen_size[0] - self.screen_size[0] % self.cell_size[0])
+        space_y = int(self.screen_size[1] - self.screen_size[1] % self.cell_size[1])
+
+        def generate_coord():
+            x = randrange(0, space_x, self.cell_size[0])
+            y = randrange(0, space_y, self.cell_size[1])
+            return x, y
+
+        while True:
+            coord = generate_coord()
+            if coord not in self.snake.snake_list:
+                return coord
+
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                self.__check_keydown_events(event)
+                self.check_keydown_events(event)
 
-    def __check_keydown_events(self, event: pygame.event):
+    def check_keydown_events(self, event: pygame.event):
         key = event.key
         directions = {
             pygame.K_w: (0, -1),
@@ -47,8 +66,9 @@ class Game:
     def update_screen(self):
         self.screen.fill('gray')
         self.snake.draw(self.screen)
-        self.clock.tick(self.frame)
+        self.fruit.draw(self.screen)
         pygame.display.flip()
+        self.clock.tick(self.frame)
 
 
 if __name__ == '__main__':
